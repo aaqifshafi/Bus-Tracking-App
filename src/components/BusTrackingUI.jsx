f048a30a2fe0d781c252dc56c001440f20a8ce49
faaridzargar <zargarfaarid@gmail.com>
2024-06-28 00:45:39 +0530

    added CSS

--- src/components/BusTrackingUI.jsx
+++ src/components/BusTrackingUI.jsx
@@ -8,7 +8,7 @@ const BusTrackingUI = () => {
       {/* Insert your desired description here */}
       <div className="map-container">
         <Map
-          mapContainerStyle={{ width: "100%", height: "400px" }}
+          // mapContainerStyle={{ width: "100%", height: "400px" }}
           center={{ lat: 40.7484, lng: -73.9857 }}
           zoom={12}
         />
