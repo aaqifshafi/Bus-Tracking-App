a4728923ce505e84a5f6f6962a012c182d407c4c
faaridzargar <zargarfaarid@gmail.com>
2024-06-29 12:43:31 +0530

    Add Card and details

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,16 @@ import BusTrackingUI from "@/components/BusTrackingUI";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Test from "@/components/Test";
+import {Card} from "@/components/ui/card"
 
 export default function page() {
   return (
     <>
       <Navbar />
-      <Test />
+      <Card>
       <BusTrackingUI />
+      </Card>
+      
       <Footer />
     </>
   );
--- src/components/BusTrackingUI.jsx
+++ src/components/BusTrackingUI.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "@/styles/BusTrackingUI.css";
+
 import Map from "./Map";
 
 
@@ -8,9 +9,29 @@ const BusTrackingUI = () => {
     <div className="bus-tracking-container">
       {/* Insert your desired description here */}
       <div className="map-container">
-
-        <Map/>
-
+          
+            <Map/>
+      </div>
+      <div className="details">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Details</h3><hr /><br />
+        <ul>
+          <li>Bus Name:
+            <p>B-21s</p> </li><br />
+          <li>Bus Number: 
+            <p>JK01 7777</p><br />
+          </li>
+          <li>Bus Location: 
+            <p>Bus is at the bus stop</p><br />
+          </li>
+        </ul>
+        <ul>
+          <li>Driver Name:
+            <p>Rashid Ahmad</p><br />
+          </li>
+          <li>Driver Number:
+            <p>999999900</p><br />
+          </li>
+        </ul>
       </div>
     </div>
   );
