96c0b2a8be41603029435a5bde99aa82e3156d3d
Aaqif Shafi <63208372+aaqifshafi@users.noreply.github.com>
2024-06-29 11:51:01 +0530

    Deleted Test Comoponent

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import BusTrackingUI from "@/components/BusTrackingUI";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import Test from "@/components/Test";
 
 export default function page() {
   return (
     <>
       <Navbar />
-      {/* <Test /> */}
       <BusTrackingUI />
       <Footer />
     </>
