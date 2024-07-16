e26e47f39cafaaeca861375a13326cc24bc76482
Aaqif Shafi <63208372+aaqifshafi@users.noreply.github.com>
2024-07-16 15:25:26 +0530

    dashboard fixed

--- src/components/BusInfo.jsx
+++ src/components/BusInfo.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const BusInfo = ({ busData }) => {
   return (
-    <div className="h-full font-medium self-start justify-self-start md:self-center md:justify-self-center">
+    <div className="h-full font-medium self-center justify-self-center">
       <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
         Details
       </h3>
       <hr />
+
       <ul className="my-2 list-disc [&>li]:m-6">
         <li>
           Bus Name:
--- src/components/BusTrackingUI.jsx
+++ src/components/BusTrackingUI.jsx
@@ -9,16 +9,12 @@ const BusTrackingUI = () => {
   const [busData, setBusData] = useState(null);
   useEffect(() => {}, [busData]);
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-[1fr_2fr]">
-      {busData && (
-        <div className="order-2 md:order-1 md:pr-4 md:pt-0">
-          <BusInfo busData={busData} />
-        </div>
-      )}
-      <div className="w-full h-96 md:h-80 lg:h-full flex justify-center items-center border-2 border-muted rounded-[20px] overflow-hidden order-1 md:order-2 p-2 md:p-4">
+    <>
+      <div className="w-full h-full flex justify-center items-center border-2 border-muted rounded-[20px] overflow-hidden self-center justify-self-center">
         <Map setBusData={setBusData} />
       </div>
-    </div>
+      {busData && <BusInfo busData={busData} />}
+    </>
   );
 };
 
