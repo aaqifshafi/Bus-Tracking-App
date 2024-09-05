d0b10e81fdcd5badead33e5af0e6e6558b45217d
faaridzargar <zargarfaarid@gmail.com>
2024-09-05 13:11:51 +0530

    Refactor:Dashboards

--- src/components/BusTrackingUI.jsx
+++ src/components/BusTrackingUI.jsx
@@ -10,10 +10,12 @@ const BusTrackingUI = () => {
   useEffect(() => {}, [busData]);
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center border-2 border-muted rounded-[20px] overflow-hidden self-center justify-self-center">
+      <div className="w-full h-[400px] border-2 border-muted rounded-[20px] overflow-hidden self-center justify-self-center md:w-[500px]">
         <Map setBusData={setBusData} />
       </div>
+      <div>
       {busData && <BusInfo busData={busData} />}
+      </div>
     </>
   );
 };
